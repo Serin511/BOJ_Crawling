88ced156f01061d1eb4fa1d978565baca1d1e131
Serin Kim <61223466+dsstar-codes@users.noreply.github.com>
2022-05-03 22:59:25 +0900

    Update submit_codes.py

--- submit_codes.py
+++ submit_codes.py
@@ -1,5 +1,5 @@
 bojid = "bojid"
-yourpassword = "yourpassword"
+yourpassword = "12345678"
 
 import requests
 import os
@@ -12,7 +12,6 @@ from selenium.webdriver.common.keys import Keys
 import clipboard
 
 LANG = {"C++20":95, "C++11":49, "C++17":84, "C++14":88, "C++20 (Clang)":96, "Python 3":28, "PyPy3":73, "Text":58}
-#딴건 알아서~
 
 path = 'codes/'
 file_list = os.listdir(path)
@@ -29,16 +28,21 @@ driver.find_element_by_xpath('//*[@id="login_form"]/div[2]/input').send_keys(boj
 driver.find_element_by_xpath('//*[@id="login_form"]/div[3]/input').send_keys(yourpassword)
 time.sleep(10)
 
+fl=0
+
 for filename in file_list:
 	file = open(path+filename, "r")
 	num,lang = filename.split('.')
 	driver.get("https://www.acmicpc.net/submit/"+num)
-	time.sleep(3)
+	time.sleep(5)
 
 	code = str(file.read())	
 	clipboard.copy(code)
 	element = driver.switch_to.active_element
-	element.send_keys(Keys.CONTROL, 'v')
+	element.send_keys(Keys.CONTROL+'a')
+	element.send_keys(Keys.CONTROL+'v')
+
+	time.sleep(2)
 
 	langbox = driver.find_element_by_xpath('//*[@id="language_chosen"]/div/div/input')
 	langbox.send_keys(lang+Keys.ENTER)
@@ -46,4 +50,4 @@ for filename in file_list:
 	driver.find_element_by_xpath('//*[@id="submit_button"]').click()
 
 	file.close()
-	time.sleep(20)
+	time.sleep(5)
