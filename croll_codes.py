625b4697b3dcadefaaf7296ab722b03e5d9e651e
Seokpyo Kim <61223466+dsstar-codes@users.noreply.github.com>
2022-05-03 15:51:30 +0900

    Update croll_codes.py

--- croll_codes.py
+++ croll_codes.py
@@ -29,7 +29,7 @@ for dt in arr:
 	nums.append(dt.get_attribute('innerText'))
 
 for num in nums:
-	driver.get('https://www.acmicpc.net/status?problem_id='+num+'&user_id=dennisstar&language_id=-1&result_id=4&from_mine=1')
+	driver.get('https://www.acmicpc.net/status?problem_id='+num+'&user_id='+bojid+'&language_id=-1&result_id=4&from_mine=1')
 	driver.implicitly_wait(3)
 	accodes = driver.find_elements_by_xpath('//*[@id="status-table"]/tbody/tr')
 	langurl,codeurl = accodes[-1].find_elements_by_xpath('td[7]/a')
